c110ef1a79b549ba488ba5e9ac107248be3dafae
Ray <emanual0426@gmail.com>
2018-03-14 16:23:53 +0800

    2rd commit

---
+++ ch2/ch2_18_decision_tree.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 15:59:52 2018
+
+@author: Yuan-Ray Chang
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+tree = mglearn.plots.plot_tree_not_monotone()
+display(tree)
---
+++ ch2/ch2_19_decision_tree_regressor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 16:06:56 2018
+
+@author: Yuan-Ray Chang
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
