96a52982dfd165356d3b3698894e7898836353ee
Ray <emanual0426@gmail.com>
2018-03-15 10:03:03 +0800

    change name

---
+++ ch2/ch2_21_Gradient_Boosting_Classifier.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 09:48:30 2018
+
+@author: Yuan-Ray Chang
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, \
+        cancer.target, random_state=0)
+
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gbrt.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
+
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gbrt.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
+
+gbrt = GradientBoostingClassifier(random_state=0, learning_rate=0.01)
+gbrt.fit(X_train, y_train)
+print("Accuracy on training set: {:.3f}".format(gbrt.score(X_train, y_train)))
+print("Accuracy on test set: {:.3f}".format(gbrt.score(X_test, y_test)))
+
+gbrt = GradientBoostingClassifier(random_state=0, max_depth=1)
+gbrt.fit(X_train, y_train)
+plot_feature_importances_cancer(gbrt)
