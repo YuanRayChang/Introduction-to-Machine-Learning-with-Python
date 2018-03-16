22b79b56100b081c7fe6e551fa0fba99ce935f42
Ray <emanual0426@gmail.com>
2018-03-16 11:38:36 +0800

    fix

--- ch2/ch2_30_Predicting_Probabilities.py
+++ ch2/ch2_30_Predicting_Probabilities.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 16 08:55:27 2018
+Created on Thu Mar 15 20:20:13 2018
 
 @author: Yuan-Ray Chang
 """
@@ -10,36 +10,38 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import mglearn
 
-from sklearn.datasets import load_iris
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import make_circles
 
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, \
-            iris.target, random_state=42)
-gbrt = GradientBoostingClassifier(learning_rate=0.01, random_state=0)
-gbrt.fit(X_train, y_train)
+X, y = make_circles(noise=0.25, factor=0.5, random_state=1)
 
-print("Decision function shape: {}".format(gbrt.decision_function(X_test).shape))
-print("Decision function: {}".format(gbrt.decision_function(X_test[:6,:])))
+y_named = np.array(["blue", "red"])[y]
 
-print("Argmax of decision function:\n{}".format(\
-      np.argmax(gbrt.decision_function(X_test), axis=1)))
-print("Predictions:\n{}".format(gbrt.predict(X_test)))
+X_train, X_test, y_train_named, y_test_named, y_train, y_test = \
+        train_test_split(X, y_named, y, random_state=0)
 
+gbrt = GradientBoostingClassifier(random_state=0)
+gbrt.fit(X_train, y_train_named)
+
+print("Shape of probabilities: {}".format(gbrt.predict_proba(X_test).shape))
 print("Predicted probabilities:\n{}".format(gbrt.predict_proba(X_test[:6])))
-print("Sums: {}".format(gbrt.predict_proba(X_test[:6]).sum(axis=1)))
 
-print("Argmax of predicted probabilities:\n{}".format(\
-      np.argmax(gbrt.predict_proba(X_test), axis=1)))
-print("Predictions:\n{}".format(gbrt.predict(X_test)))
-
-logreg = LogisticRegression()
-named_target = iris.target_names[y_train]
-logreg.fit(X_train, named_target)
-print("unique classes in training data: {}".format(logreg.classes_))
-print("predictions: {}".format(logreg.predict(X_test)[:10]))
-argmax_dec_func = np.argmax(logreg.decision_function(X_test), axis=1)
-print("argmax of decision function: {}".format(argmax_dec_func[:10]))
-print("argmax combined with classes_: {}".format(\
-      logreg.classes_[argmax_dec_func][:10]))
+fig, axes = plt.subplots(1, 2, figsize=(13, 5))
+mglearn.tools.plot_2d_separator(gbrt, X, ax=axes[0], alpha=0.4, \
+        fill=True, cm=mglearn.cm2)
+scores_image = mglearn.tools.plot_2d_scores(gbrt, X, ax=axes[1], \
+        alpha=0.5, cm=mglearn.ReBl, function='predict_proba')
+for ax in axes:
+    mglearn.discrete_scatter(X_test[:,0], X_test[:,1], y_test, \
+                             markers='^', ax=ax)
+    mglearn.discrete_scatter(X_train[:,0], X_train[:,1], y_train, \
+                             markers='o', ax=ax)
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+cbar = plt.colorbar(scores_image, ax=axes.tolist())
+axes[0].legend(["Test class 0", "Test class 1", "Train class 0", \
+    "Train class 1"], ncol=4, loc=(0.1, 1.1))
+    
+    
+    
+    
