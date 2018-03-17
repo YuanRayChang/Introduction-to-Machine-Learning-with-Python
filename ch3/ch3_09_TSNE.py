5e3d6480a94651cea737c54db0e1b7862cc18395
Ray <emanual0426@gmail.com>
2018-03-17 10:34:01 +0800

    add

--- ch3/ch3_09_TSNE.py
+++ ch3/ch3_09_TSNE.py
@@ -9,3 +9,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import mglearn
+
+from sklearn.decomposition import PCA
+from sklearn.datasets import load_digits
+from sklearn.manifold import TSNE
+digits = load_digits()
+
+fig, axes = plt.subplots(2, 5, figsize=(10, 5), \
+                subplot_kw={'xticks': (), 'yticks': ()})
+for ax, img in zip(axes.ravel(), digits.images):
+    ax.imshow(img)
+
+pca = PCA(n_components=2)
+pca.fit(digits.data)
+digits_pca = pca.transform(digits.data)
+colors = ["#476A2A", "#7851B8", "#BD3430", "#4A2D4E", \
+          "#875525", "#A83683", "#4E655E", "#853541", \
+          "#3A3120", "#535D8E"]
+plt.figure(figsize=(10, 10))
+plt.xlim(digits_pca[:, 0].min(), digits_pca[:, 0].max())
+plt.ylim(digits_pca[:, 1].min(), digits_pca[:, 1].max())
+for i in range(len(digits.data)):
+    plt.text(digits_pca[i, 0], digits_pca[i, 1], \
+    str(digits.target[i]), color = colors[digits.target[i]], \
+    fontdict={'weight': 'bold', 'size': 9})
+plt.xlabel("First principal component")
+plt.ylabel("Second principal component")
+
+tsne = TSNE(random_state=42)
+digits_tsne = tsne.fit_transform(digits.data)
+plt.figure(figsize=(10, 10))
+plt.xlim(digits_tsne[:, 0].min(), digits_tsne[:, 0].max() + 1)
+plt.ylim(digits_tsne[:, 1].min(), digits_tsne[:, 1].max() + 1)
+for i in range(len(digits.data)):
+    plt.text(digits_tsne[i, 0], digits_tsne[i, 1], \
+    str(digits.target[i]), color = colors[digits.target[i]], \
+    fontdict={'weight': 'bold', 'size': 9})
+plt.xlabel("t-SNE feature 0")
+plt.ylabel("t-SNE feature 1")
+
+
+
+
+
+
+
+
+
+
---
+++ ch3/ch3_10_kMeans.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 17 10:33:20 2018
+
+@author: Yuan-Ray Chang
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+mglearn.plots.plot_kmeans_algorithm()
