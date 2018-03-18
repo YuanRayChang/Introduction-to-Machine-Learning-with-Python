aa1dd4b19678eb7d8df91a75ceaa89b9f4eb92ab
Ray <emanual0426@gmail.com>
2018-03-18 18:39:06 +0800

    add

---
+++ ch3/ch3_20.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 18 17:08:19 2018
+
+@author: Yuan-Ray Chang
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+from sklearn.datasets import fetch_lfw_people
+people = fetch_lfw_people(min_faces_per_person=20, resize=0.7)
+image_shape = people.images[0].shape
+mask = np.zeros(people.target.shape, dtype=np.bool)
+for target in np.unique(people.target):
+    mask[np.where(people.target == target)[0][:50]] = 1
+X_people = people.data[mask]
+y_people = people.target[mask]
+X_people = X_people / 255.
+
+
+pca = PCA(n_components=100, whiten=True, random_state=0)
+pca.fit_transform(X_people)
+X_pca = pca.transform(X_people)
+
+dbscan = DBSCAN()
+labels = dbscan.fit_predict(X_pca)
+print("Unique labels: {}".format(np.unique(labels)))
+
+dbscan = DBSCAN(min_samples=3)
+labels = dbscan.fit_predict(X_pca)
+print("Unique labels: {}".format(np.unique(labels)))
+
+dbscan = DBSCAN(min_samples=3, eps=15)
+labels = dbscan.fit_predict(X_pca)
+print("Unique labels: {}".format(np.unique(labels)))
+
+# count the number of noise(-1) and cluster1(0)
+# arguments in bincounts can't be negative
+# [number_of_noise number_of_cluster1]
+print("Number of points per cluster: {}".format(\
+      np.bincount(labels + 1)))
+
+noise = X_people[labels==-1]
+
+fig, axes = plt.subplots(3, 9, subplot_kw={'xticks': (), \
+            'yticks': ()}, figsize=(12, 4))
+for image, ax in zip(noise, axes.ravel()):
+    ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+
+
+for eps in[1, 3, 5, 7, 9, 11, 13]:
+    print("\neps={}".format(eps))
+    dbscan = DBSCAN(eps=eps, min_samples=3)
+    labels = dbscan.fit_predict(X_pca)
+    print("Clusters present: {}".format(np.unique(labels)))
+    print("Cluster sizes: {}".format(np.bincount(labels + 1)))
+    
+dbscan = DBSCAN(min_samples=3, eps=7)
+labels = dbscan.fit_predict(X_pca)
+
+for cluster in range(max(labels) + 1):
+   mask = labels == cluster
+   n_images = np.sum(mask)
+   fig, axes = plt.subplots(1, n_images, \
+                figsize=(n_images * 1.5, 4), \
+                subplot_kw={'xticks': (), 'yticks': ()})
+   for image, label, ax in zip(X_people[mask], \
+                               y_people[mask], axes):
+       ax.imshow(image.reshape(image_shape), vmin=0, vmax=1)
+       ax.set_title(people.target_names[label].split()[-1])
