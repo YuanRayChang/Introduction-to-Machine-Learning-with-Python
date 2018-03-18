83d69d2efa126088abbc4a1ab60da881888dd61f
Ray <emanual0426@gmail.com>
2018-03-18 12:19:02 +0800

    add

--- ch2/ch2_05_knn_regression.py
+++ ch2/ch2_05_knn_regression.py
@@ -14,6 +14,8 @@ mglearn.plots.plot_knn_regression(n_neighbors=1)
 mglearn.plots.plot_knn_regression(n_neighbors=3)
 
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.model_selection import train_test_split
+
 X, y = mglearn.datasets.make_wave(n_samples=40)
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 reg = KNeighborsRegressor(n_neighbors=3)
